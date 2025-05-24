d9fc6619923f48d3c7e2b85f62effc2bde95bf39
mateuslfelicio <mateuslfvrb@hotmail.com>
2025-05-23 22:39:12 -0300

    cria cenario e começa a config a camera

---
+++ T1/createArea.js
@@ -0,0 +1,136 @@
+import * as THREE from  'three';
+import Stats from '../build/jsm/libs/stats.module.js';
+import {PointerLockControls} from '../build/jsm/controls/PointerLockControls.js';
+import {initRenderer, 
+        initCamera,
+        initDefaultBasicLight,
+        setDefaultMaterial,
+        InfoBox,
+        onWindowResize,
+        createGroundPlaneXZ,
+        createGroundPlane} from "../libs/util/util.js";
+
+
+export class Area {
+
+  constructor (scene){
+    this.scene = scene;
+  }
+
+  static createMap(scene){
+    let positions = [];
+    
+    positions.push(new THREE.Vector3(-160.0, 12.0, -172.0));
+    positions.push(new THREE.Vector3(0.0, 12.0, -172.0));
+    positions.push(new THREE.Vector3(160.0, 12.0, -172.0));
+    positions.push(new THREE.Vector3(0.0, 12.0, 172.0));
+    
+    this.createTerrain(scene);
+
+    for(let i = 0; i < positions.length; i++){
+      this.createArea(scene, positions[i], i);
+    }
+  }
+
+  static createArea(scene, position, i) {
+    let material = setDefaultMaterial();
+    //criação da base da plataforma
+    //inicialização com valores padrão (plataforma pequena com escada ao centro)
+    const heigh = 24.0;
+    let length = 120.0;
+    let leftLength = 48.0;
+    let rightLength = 48.0;
+    
+    if(i == 0){
+      leftLength = 16.0;
+      rightLength = 80.0;
+    }
+    if(i == 1){
+      leftLength = 80.0;
+      rightLength = 16.0;
+    }
+    if(i == 2){
+      leftLength = 48.0;
+      rightLength = 48.0;
+    }
+    if(i == 3){
+      length = 360.0;
+      leftLength = 167.5;
+      rightLength = 167.5;
+    }
+
+
+    let cubeGeometry = new THREE.BoxGeometry(length, heigh, 96.0);
+    let cube = new THREE.Mesh(cubeGeometry, material);
+    cube.position.copy(position);
+    if(i == 3)
+      cube.rotation.y = Math.PI;
+    scene.add(cube);
+
+    //cubos laterais
+    let cubeGeometry2 = new THREE.BoxGeometry(leftLength, heigh, heigh);
+    let cubeLeft = new THREE.Mesh(cubeGeometry2, material);
+    //calculo da posição do cubo esquerdo em relação ao cubo principal
+    cubeLeft.position.set(-(length-leftLength)/2, 0.0, 60.0);
+    cube.add(cubeLeft);
+
+    let cubeGeometry3 = new THREE.BoxGeometry(rightLength, heigh, heigh);
+    let cubeRight = new THREE.Mesh(cubeGeometry3, material);
+    cubeRight.position.set((length-rightLength)/2, 0.0, 60.0);
+    cube.add(cubeRight);
+    
+    
+    //escadas
+    let stairHeigh = heigh/8;
+    //calculo da posição da escada em relação ao cubo principal
+    let stairPositionX = 0.0;
+    if(leftLength < length/2){
+      stairPositionX = -(length - leftLength) / 2 + leftLength / 2 + 12.5;
+    } else {
+      stairPositionX = (length - rightLength) / 2 - rightLength / 2 - 12.5;
+    }
+    //reescrever/deixar mais legivel se possivel
+    //os numeros são correções para a escada ficar alinhada ao cubo principal
+    for (let i = 0; i < 8; i++) {
+      let stairStepGeometry = new THREE.BoxGeometry( 25.0, stairHeigh, stairHeigh * (1 + 7 - i));
+      let stairStep = new THREE.Mesh(stairStepGeometry, material);
+      if (i == 0) {
+        stairStep.position.set(stairPositionX, -10.5, 60);
+      } else {
+        stairStep.position.set(stairPositionX, -10.5+stairHeigh * i, 60 - (stairHeigh * i) / 2);
+      }
+      cube.add(stairStep);
+    }
+  }
+
+  static createTerrain(scene){
+    //create walls
+    let wallGeometry = new THREE.PlaneGeometry(500, 72); 
+    let wallMaterial = new THREE.MeshBasicMaterial();
+
+    let walls = [];
+
+    for(let i = 0; i < 4; ++i){
+        walls.push(new THREE.Mesh(wallGeometry,wallMaterial));
+    }
+
+    walls[0].position.set(0, 36, -250);
+
+    walls[1].position.set(-250, 36, 0);
+    walls[1].rotation.y = Math.PI / 2;
+
+    walls[2].position.set(250, 36, 0);
+    walls[2].rotation.y = Math.PI / -2;
+
+    walls[3].position.set(0, 36, 250);
+    walls[3].rotation.y = Math.PI
+
+    for(let i = 0; i < walls.length; ++i){
+      scene.add(walls[i]);
+    }
+
+    // create the ground plane
+    let plane = createGroundPlaneXZ(510, 510);
+    scene.add(plane);
+  }
+}
