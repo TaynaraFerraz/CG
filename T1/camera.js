61793f99139fde032a33323f57e05cb99cf9a3a9
mateuslf <mateuslfvrb@hotmail.com>
2025-05-26 17:24:11 -0300

    ajustes menores

--- T1/camera.js
+++ T1/camera.js
@@ -24,12 +24,11 @@ light = initDefaultBasicLight(scene); // Create a basic light to illuminate the 
 
 //inicio da configuração da camera
 camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 17, 0); //posição no mundo
+camera.position.set(0, 17, 0);
 camera.lookAt(new THREE.Vector3(0.0, 1.0, -100.0));
 
 
 //criando o camera holder
-
 let cameraHolderGeometry = new THREE.CylinderGeometry(4, 4, 17);
 cameraHolder = new THREE.Mesh(cameraHolderGeometry, material);
 cameraHolder.position.set(0, 20, 50);
