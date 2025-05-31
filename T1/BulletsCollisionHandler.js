a8497324cfbcabd56e0b4c14db100bdc322b7531
Felipe Souza <120433923+felipeosouza@users.noreply.github.com>
2025-05-31 13:52:49 -0300

    remove logs

--- T1/BulletsCollisionHandler.js
+++ T1/BulletsCollisionHandler.js
@@ -2,9 +2,7 @@ import * as THREE from 'three';
 import {
     setDefaultMaterial,
 } from "../libs/util/util.js";
-import { Vector3 } from '../build/three.module.js';
 
-export const spheres = []
 export class BulletsCollisionHandler {
     #spheres = []
     #camera;
@@ -12,22 +10,12 @@ export class BulletsCollisionHandler {
     position
     direction
     move
-    speed = 5;
-    prevPosition
-    // #spheres = [];
+    speed = 4.5;
+    prevPosition;
 
     constructor(scene, camera) {
-        const sphereGeometry = new THREE.SphereGeometry(0.2, 32, 16);
-        const materialSphere = setDefaultMaterial('lightblue');
-        this.sphere = new THREE.Mesh(sphereGeometry, materialSphere);
-        spheres.push(this) // instancia ou inves de armazenar a mesh
-        scene.add(this.sphere);
-        console.log(spheres.length)
         this.#camera = camera;
         this.#scene = scene;
-
-        // Define posição inicial
-        //this.sphere.position.copy(position);
 
         // Direção baseada na câmera
         const dir = new THREE.Vector3();
@@ -39,10 +27,6 @@ export class BulletsCollisionHandler {
 
     #updateSpherePosition(sphere) {
             sphere.translateZ(-this.speed);
-            /* this.prevPosition.copy(this.sphere.position);
-            const velocity = this.direction.clone().multiplyScalar(this.speed);
-            this.sphere.position.add(velocity);    // this.sphere.translateZ(-2) fez com que a bolinha seja disparada para uma unica direção, bolinha não estava girando junto com a câmera
-            console.log(this.sphere.position); */
     }
 
     remove(scene) {
@@ -56,29 +40,23 @@ export class BulletsCollisionHandler {
         let sphereLookAt = new THREE.Vector3();
         sphere.getWorldPosition(sphereLookAt);
 
-        console.log("pos atual", sphereLookAt);
-        console.log("direcao", this.direction);
         this.direction.multiplyScalar(-4);
         sphereLookAt.add(this.direction);
         this.direction.multiplyScalar(-1/4);
 
-        console.log("final", sphereLookAt);
-
         sphere.lookAt(sphereLookAt);
+        sphere.translateZ(-0.3); //para sair da boca do cilindro e não do meio
     }
 
     handleBulletsCollisions(collidables) {
         this.#camera.getWorldDirection(this.direction);
-        //console.log(this.direction);
         
-        this.#spheres = this.#spheres.filter((s) => {
-            //console.log(s);
-            
-            const prevPositionBall = s.position.clone();
+        this.#spheres = this.#spheres.filter((sphere) => {  
+            const prevPositionBall = sphere.position.clone();
 
-            this.#updateSpherePosition(s);
+            this.#updateSpherePosition(sphere);
 
-            const currPositionBall = s.position.clone();
+            const currPositionBall = sphere.position.clone();
             const directionBall = new THREE.Vector3().subVectors(currPositionBall, prevPositionBall).normalize();
             const distanceBall = prevPositionBall.distanceTo(currPositionBall);
 
@@ -90,20 +68,16 @@ export class BulletsCollisionHandler {
             const raycasterBall = new THREE.Raycaster(prevPositionBall, directionBall, 0, distanceBall);
             const intersectsBall = raycasterBall.intersectObjects(collidableMeshes, true);
 
-            if (intersectsBall.length > 0 || s.position.y >= 72 || s.position.y <= 0) {
-                console.log("colidiu")
-                console.log('position', s.position)
-                this.#scene.remove(s);
-                s.geometry.dispose();
-                s.material.dispose();
-                s = undefined;
+            if (intersectsBall.length > 0 || sphere.position.y >= 72 || sphere.position.y <= 0) {
+                this.#scene.remove(sphere);
+                sphere.geometry.dispose();
+                sphere.material.dispose();
+                sphere = undefined;
                 return false;
             }
-            console.log(this.#spheres.length)
             
             return true;
         })
-        //console.log(this.#spheres.length)
         this.prevPosition = this.#camera.position;
     }
 }
