3ba5700df06943be2356ca116bc412ebd4fea4e8
TaynaraFerraz <taynaracferraz@gmail.com>
2025-06-30 20:11:16 -0300

    inicio da função de criar a chave

--- T2/camera.js
+++ T2/camera.js
@@ -11,6 +11,7 @@ import {
 import { PlayerCollisionHandler } from './PlayerCollisionHandler.js';
 import { BulletsCollisionHandler } from './BulletsCollisionHandler.js';
 import { PLAYER_HEIGHT, PLAYER_WIDTH, SHIFT_MULTIPLIER, SPEED } from './constants.js';
+import { CSG } from '../libs/other/CSGMesh.js';
 
 const clock = new THREE.Clock();
 let scene, renderer, camera, cameraHolder, material, light, keyboard; // Initial variables
@@ -38,7 +39,7 @@ camera.add(arma);
 //criando o camera holder
 let cameraHolderGeometry = new THREE.CylinderGeometry(PLAYER_WIDTH, PLAYER_WIDTH, PLAYER_HEIGHT);
 cameraHolder = new THREE.Mesh(cameraHolderGeometry, material);
-cameraHolder.position.set(-120, PLAYER_HEIGHT+5, -120);
+cameraHolder.position.set(-120, PLAYER_HEIGHT + 5, -120);
 
 cameraHolder.add(camera);
 
@@ -82,6 +83,46 @@ let moveRight = false;
 window.addEventListener('keydown', (event) => movementControls(event.keyCode, true));
 window.addEventListener('keyup', (event) => movementControls(event.keyCode, false));
 
+function createKey() {
+    let cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
+    let cube = new THREE.Mesh(cubeGeometry);
+
+    let cylinderGeometry = new THREE.CylinderGeometry(0.6, 0.6, 2.5);
+    let cylinder1 = new THREE.Mesh(cylinderGeometry);
+    let cylinder2 = new THREE.Mesh(cylinderGeometry);
+    let cylinder3 = new THREE.Mesh(cylinderGeometry);
+
+    cylinder2.rotateX(Math.PI / 2);
+    cylinder3.rotateZ(-Math.PI / 2);
+
+    cylinder1.matrixAutoUpdate = false;
+    cylinder2.matrixAutoUpdate = false;
+    cylinder3.matrixAutoUpdate = false;
+
+    cylinder1.updateMatrix();
+    cylinder2.updateMatrix();
+    cylinder3.updateMatrix();
+
+    let cubeCSG = CSG.fromMesh(cube);
+    let cylinder1CSG = CSG.fromMesh(cylinder1)
+    let cylinder2CSG = CSG.fromMesh(cylinder2)
+    let cylinder3CSG = CSG.fromMesh(cylinder3)
+
+    let csgObject = cubeCSG.subtract(cylinder1CSG);
+    let csgObject1 = csgObject.subtract(cylinder2CSG);
+    let csgObject2 = csgObject1.subtract(cylinder3CSG);
+    let csgFinal = CSG.toMesh(csgObject2, new THREE.Matrix4())
+    csgFinal.material = new THREE.MeshPhongMaterial({
+        "color": "rgb(223, 47, 47)",
+        "shininess": "200",
+        "specular": "rgb(255,255,255)"
+    })
+
+    csgFinal.scale.set(0.3, 0.3, 0.3);
+    csgFinal.position.set(-160.0, 6.9, -162.0);
+    scene.add(csgFinal);
+}
+
 //mapeia as teclas para os movimentos
 function movementControls(key, value) {
     switch (key) {
@@ -104,13 +145,13 @@ function movementControls(key, value) {
         case 39: // Seta pra direita
             moveRight = value;
             break;
-        }
+    }
 }
 
 //realiza a movimentação utilizando metodos do PointerLockControls
 function moveAnimate(delta) {
     let moveSpeed = shift ? SHIFT_MULTIPLIER * SPEED * delta : SPEED * delta;
-    
+
     if (moveForward) {
         controls.moveForward(moveSpeed);
     }
@@ -196,6 +237,8 @@ function render() {
 
     //lidando com as colisões
     playerCollisionHandler.handleCollisions();
+
+    createKey();
 
     requestAnimationFrame(render);
     renderer.render(scene, camera) // Render scene
